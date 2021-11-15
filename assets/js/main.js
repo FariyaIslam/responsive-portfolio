a205b43a628f27e06dc822912f190acc8c6e46b2
Fariya Islam <fariyaislam2096@gmail.com>
2021-11-15 23:47:56 +0600

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,38 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navtoggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
 
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if (navtoggle) {
+    navtoggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
 
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*==================== ACCORDION SKILLS ====================*/
@@ -30,10 +53,10 @@
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
