4fa5f12bd4f258d48cdb3cb6547e3083cce7637c
Fariya Islam <fariyaislam2096@gmail.com>
2021-12-10 21:08:24 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,6 +39,17 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const skillsContent = document.getElementsByClassName('skills__content'),
     skillsHeader = document.querySelector('.skills__header')
 
+function toggleskills()
+    {
+        let itemClass = this.parentNode.getElementsByClassName
+        for(i=0; i<skillsContent.length; i++){
+            skillsContent[i].className = 'skills__content skills__close'
+        }
+        if(itemClass === 'skills__content skills__close'){
+            this.parentNode.className = 'skills__content skills__open'
+        }
+    }
+
 
 /*==================== QUALIFICATION TABS ====================*/
 
