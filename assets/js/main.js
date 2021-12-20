3b173594138ba6c8e1f3547350d638c90ff060b5
Fariya Islam <fariyaislam2096@gmail.com>
2021-12-20 20:00:50 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ const skillsContent = document.getElementsByClassName('skills__content'),
 function toggleskills() {
     let itemClass = this.parentNode.className
     for (i = 0; i < skillsContent.length; i++) {
-        skillsContent[i].className = 'skills__content skills__close'
+         skillsContent[i].className = 'skills__content skills__close'
     }
     if (itemClass === 'skills__content skills__close') {
        this.parentNode.className = 'skills__content skills__open'
