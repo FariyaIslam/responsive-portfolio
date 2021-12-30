253c131cee1c43b31b14fbd6dbf1a858c18d166e
Fariya Islam <fariyaislam2096@gmail.com>
2021-12-30 18:35:38 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,9 +44,11 @@ function toggleskills() {
     for (i = 0; i < skillsContent.length; i++) {
          skillsContent[i].className = 'skills__content skills__close'
     }
+
     if (itemClass === 'skills__content skills__close') {
        this.parentNode.className = 'skills__content skills__open'
     }
+    
 }
 skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleskills)
